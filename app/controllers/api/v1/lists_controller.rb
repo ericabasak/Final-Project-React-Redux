568963a1b2a2bec88f34cfb8290146c6246a77f9
ericabasak <ericabasak@gmail.com>
2020-10-06 15:14:22 -0400

    updated the index method in the list controller to be able to show all lists for a user

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::ListsController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
-  before_filter: check_login
+  # before_action :check_login
   
   def index
-    lists = List.all
+    user = User.find_by(username: "Erica")
+    # list = List.find_by(user_id: user.id)
+    lists = List.where(:user_id => user.id)
+    # puts "--------------------------"
+    # puts lists
+    # puts "---------------------------"
+    # lists = List.all
     render json: lists
   end
 
@@ -14,18 +20,29 @@ class Api::V1::ListsController < ActionController::Base
   end
 
   def create
-    list = List.create(list_params)
-    @user = User.find(session[:user_id])
-    list.user = @user
+    x = list_params
+    puts "------------------------------------------"
+    puts x
+    puts "----------------------------------------------"
+    list = List.new(list_params)
+    puts list
+    puts "------------------------------------------"
+    list = List.new(title: x[:title], description: x[:description], 
+    is_complete: x[:is_complete])
+    user = User.find_by(username: "Erica")
+    list.user = user
+    list.save!
+    puts list.title
+    render json: list
   end
 
   private
   
-  def check_login
-    if session[:user_id].nil?
-      // redirect to login!
-    end
-  end
+  # def check_login
+  #   if session[:user_id].nil?
+  #     redirect to login!
+  #   end
+  # end
 
   def list_params
     params.require(:list).permit(:title, :description, :is_complete)
