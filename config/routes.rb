ff19b1d945c8e3c77f9973b7155aa5d1c8470960
ericabasak <ericabasak@gmail.com>
2020-11-17 23:10:00 -0500

    removed session and replaced with auth in the old session controller and changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,9 @@ Rails.application.routes.draw do
       post "/usersignupform", to: "users#create"
       post "/userloginform", to: "users#login"
 
-      post '/login', to: 'sessions#create'
-      delete '/logout', to: 'sessions#destroy'
-      get '/get_current_user', to: 'sessions#get_current_user'
+      post '/login', to: 'auth#create'
+      delete '/logout', to: 'auth#destroy'
+      get '/get_current_user', to: 'auth#get_current_user'
     end
   end
 end
